edc06972f2fdbac47cd7020265ccd86619ee44a5
Defult name <default@gmail.com>
2026-02-19 13:34:15 +0530

    Update API config to live backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api', // Backend URL
+    // baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://dentaltask.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
